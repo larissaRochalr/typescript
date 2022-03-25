7c6457d21db7104b4abfd77ce41fa7b0402d6fab
Larissa.rocha <larissa.rocha@econocom.com.br>
2022-03-25 16:02:23 -0300

    projeot typescript em andamento

---
+++ app/app.ts
@@ -0,0 +1,14 @@
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('sumit', event => {
+    event.preventDefault();
+    controller.adiciona();
+
+})
+
+
+
+
+
